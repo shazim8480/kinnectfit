986fa94d7bdfe2d65958bc57aff4d11d963ed9d3
shazim8480 <troublemaker8480@gmail.com>
2023-12-17 22:20:35 +0600

    styling fixes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,5 @@
 import Image from "next/image";
 import { Montserrat } from "next/font/google";
-import { Button, ButtonGroup, Input } from "@nextui-org/react";
-import { KFButton } from "@/components/KFButton";
-import { KFInput } from "@/components/KFInput";
 
 const montserrat = Montserrat({
   weight: ["400", "500", "700", "800", "900"],
@@ -13,12 +10,6 @@ export default function Home() {
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${montserrat.className}`}
-    >
-      <KFButton color="primary" variant="shadow">
-        button
-      </KFButton>
-      <KFInput />
-      <Input />
-    </main>
+    ></main>
   );
 }

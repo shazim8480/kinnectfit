702e29bcc64a7550e03edcac15ecafe77f279541
shazim8480 <troublemaker8480@gmail.com>
2023-12-18 10:16:41 +0600

    footer style update

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import H1 from "../Custom/H1";
+
 import Link from "next/link";
 import { FiFacebook } from "react-icons/fi";
 import { FiTwitter } from "react-icons/fi";
@@ -7,35 +7,28 @@ import { FiInstagram } from "react-icons/fi";
 const Footer = () => {
   return (
     <footer className={`bg-primary w-[100%] text-white`}>
-      <div className="px-10 py-14 relative">
-        <div className="grid lg:grid-cols-4 sm:grid-cols-1  gap-10">
+      <div className="relative px-10 py-14">
+        <div className="grid gap-10 lg:grid-cols-4 sm:grid-cols-1">
           <div>
-            <H1 text="About Us"></H1>
+            {/* <H1 text="About Us"></H1> */}
             <div>
               <p>
-                We are a team of fitness enthusiasts dedicated to bringing you the best workout experience.
+                We are a team of fitness enthusiasts dedicated to bringing you
+                the best workout experience.
               </p>
             </div>
           </div>
           <div>
-            <H1 text="Quick Links"></H1>
+            {/* <H1 text="Quick Links"></H1> */}
             <div className="flex flex-col space-y-1">
-              <Link href="/">
-                Home
-              </Link>
-              <Link href="/">
-                Workouts
-              </Link>
-              <Link href="/">
-                Plans
-              </Link>
-              <Link href="/">
-                Contact Us
-              </Link>
+              <Link href="/">Home</Link>
+              <Link href="/">Workouts</Link>
+              <Link href="/">Plans</Link>
+              <Link href="/">Contact Us</Link>
             </div>
           </div>
           <div>
-            <H1 text="Contact"></H1>
+            {/* <H1 text="Contact"></H1> */}
             <div className="flex flex-col">
               <span>123 Fitness Street,</span>
               <span>Fit City, 56789</span>
@@ -43,7 +36,7 @@ const Footer = () => {
             </div>
           </div>
           <div>
-            <H1 text="Follow Us"></H1>
+            {/* <H1 text="Follow Us"></H1> */}
             <div className="flex space-x-8">
               <FiFacebook size={24} />
               <FiTwitter size={24} />
@@ -53,13 +46,11 @@ const Footer = () => {
         </div>
         <div className="absolute  left-0 right-0 h-[1px] bg-white mx-10 my-8" />
       </div>
-      <div className="flex flex-col text-center lg:flex-row lg:justify-between px-10">
-        <div>
-          © 2023 Fitness App.All rights reserved.
-        </div>
-        <div className="flex flex-col   lg:flex-row lg:gap-5 pb-10">
-          <Link href={"/"}>Privacy Policy</Link >
-          <Link href={"/"}>Terms & Conditions</Link >
+      <div className="flex flex-col px-10 text-center lg:flex-row lg:justify-between">
+        <div>© 2023 Fitness App.All rights reserved.</div>
+        <div className="flex flex-col pb-10 lg:flex-row lg:gap-5">
+          <Link href={"/"}>Privacy Policy</Link>
+          <Link href={"/"}>Terms & Conditions</Link>
         </div>
       </div>
     </footer>

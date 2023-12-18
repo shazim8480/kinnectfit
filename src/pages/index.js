64903774e219e9655c496d1e0864082d46221d03
shazim8480 <troublemaker8480@gmail.com>
2023-12-18 10:14:30 +0600

    conflict resolve in package.json

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,8 @@ import { Montserrat } from "next/font/google";
 // landing page components
 import Hero from "@/components/Hero/Hero";
 import FAQ from "@/components/FAQ/FAQ";
+import KFNavbar from "@/components/Navbar/Navbar";
+import Footer from "@/components/Footer/Footer";
 
 const montserrat = Montserrat({
   weight: ["400", "500", "700", "800", "900"],
@@ -12,8 +14,10 @@ const montserrat = Montserrat({
 export default function Home() {
   return (
     <main className={` ${montserrat.className}`}>
+      <KFNavbar />
       <Hero />
       <FAQ />
+      <Footer />
     </main>
   );
 }

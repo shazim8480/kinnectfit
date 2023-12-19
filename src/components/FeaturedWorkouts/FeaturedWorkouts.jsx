d5cb6365bb0e38fe5896ee85015fc96d8ac2d28a
shazim8480 <troublemaker8480@gmail.com>
2023-12-19 21:06:13 +0600

    ui fix for featured workouts

--- src/components/FeaturedWorkouts/FeaturedWorkouts.jsx
+++ src/components/FeaturedWorkouts/FeaturedWorkouts.jsx
@@ -7,7 +7,7 @@ import RightArrowIcon from "@/assets/icons/RightArrowIcon";
 
 const FeaturedWorkouts = () => {
   return (
-    <section className="py-8 lg:py-16">
+    <section className="pt-8 pb-8 lg:pt-24 lg:pb-16">
       <HeadingText title="Popular Workouts" className="text-center" />
       <div className="grid max-w-screen-xl grid-cols-1 gap-6 px-4 py-8 mx-auto place-items-center lg:place-content-center lg:gap-8 xl:gap-8 lg:py-16 lg:grid-cols-4">
         {workout_data.map((workoutItem, index) => (

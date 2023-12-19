a5fc890641d32b549afe04ce735b881806b20eae
shazim8480 <troublemaker8480@gmail.com>
2023-12-19 21:16:43 +0600

    hero section styles fix

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,14 +6,17 @@ import RightArrowIcon from "@/assets/icons/RightArrowIcon";
 
 const Hero = () => {
   return (
-    <section className="bg-[conic-gradient(at_bottom,_var(--tw-gradient-stops))] from-blue-800 via-blue-50 to-blue-50 dark:bg-gray-900">
+    <section className="bg-white lg:bg-[conic-gradient(at_bottom,_var(--tw-gradient-stops))] lg:from-blue-800 lg:via-blue-50 lg:to-blue-50 dark:bg-gray-900">
       <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
         <div className="mr-auto place-self-center lg:col-span-6">
-          <p className="max-w-2xl font-light text-gray-600 lg:mb-8 md:text-lg lg:text-xl">
+          <p className="max-w-2xl mb-4 font-light text-gray-600 lg:mb-8 md:text-lg lg:text-xl">
             #1 nutrition tracking app
           </p>
-          <h1 className="max-w-2xl mb-4 text-4xl font-extrabold leading-6 tracking-tight md:text-5xl xl:text-6xl">
-            Reach your goals <br /> with <span>KinnectFit</span>
+          <h1 className="relative max-w-2xl mb-4 text-4xl font-extrabold leading-none tracking-tight text-blue-950 md:text-5xl xl:text-5xl">
+            Reach your goals <br />
+            <span className="">
+              with <span className="text-blue-800">KinnectFit</span>
+            </span>
           </h1>
           <p className="max-w-2xl mb-6 font-light text-gray-600 lg:mb-8 md:text-lg lg:text-xl">
             Build healthy habits with the all-in-one food, exercise, and calorie

dc5f322c2619e100eabcb1931d5f3b0670f1de81
whoafridi <afridirk10@gmail.com>
2023-12-26 18:46:24 +0600

    workput features updated

--- src/pages/workouts/[workoutName].js
+++ src/pages/workouts/[workoutName].js
@@ -7,6 +7,7 @@ import Star from "@/assets/icons/Star";
 import { UserIcon } from "@/assets/icons/UserIcon";
 import { Checkbox } from "@nextui-org/react";
 import { KFButton } from "@/components/UI/KFButton";
+import { useEffect, useState } from "react";
 
 function WorkoutPage() {
   const router = useRouter();
@@ -14,6 +15,12 @@ function WorkoutPage() {
   const workout_deatils = workout_data.find(
     (w) => w.workout_name === workoutName
   );
+
+  const [isStarted, setIsStarted] = useState(false);
+
+  const handleStartWorkout = () => {
+    setIsStarted(true);
+  };
 
   return (
     <section className="grid max-w-screen-xl mx-auto grid-cols-1 grid-rows-1 md:grid-cols-2 py-10">
@@ -30,17 +37,19 @@ function WorkoutPage() {
           <h5 className="my-3 text-xl font-medium leading-tight text-neutral-800">
             {workout_deatils?.workout_name}
           </h5>
-          <div className="flex items-center">
-            <UserIcon fill="black" />
-            <p className="ml-1">{workout_deatils?.trainer_name}</p>
-          </div>
-          <div className="flex items-center">
-            <Clock fill="black" />
-            <p className="ml-1">{workout_deatils?.total_workout_time} min</p>
-          </div>
-          <div className="flex items-center">
-            <Star fill="black" />
-            <p className="ml-1">{workout_deatils?.average_rating}/5</p>
+          <div className="flex gap-3">
+            <div className="flex items-center">
+              <UserIcon fill="black" />
+              <p className="ml-1">{workout_deatils?.trainer_name}</p>
+            </div>
+            <div className="flex items-center">
+              <Clock fill="black" />
+              <p className="ml-1">{workout_deatils?.total_workout_time} min</p>
+            </div>
+            <div className="flex items-center">
+              <Star fill="black" />
+              <p className="ml-1">{workout_deatils?.average_rating}/5</p>
+            </div>
           </div>
           <h5 className="my-5 text-xl font-medium leading-tight text-neutral-800">
             Workout Description
@@ -56,7 +65,7 @@ function WorkoutPage() {
           <h5 className="mb-2 text-xl font-medium leading-tight text-neutral-800">
             Workout Overview
           </h5>
-          <p className="mb-4 text-base text-neutral-600 ">
+          <div className="mb-4 text-base text-neutral-600">
             {workout_deatils?.workout_modules.map((module) => {
               return (
                 <p
@@ -64,12 +73,18 @@ function WorkoutPage() {
                   className="flex justify-between mb-4 text-base text-black p-6 rounded-lg shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)]"
                 >
                   {module.name} - {module.time} seconds
-                  <Checkbox color="secondary" />
+                  <Checkbox color="secondary" isDisabled={!isStarted} />
                 </p>
               );
             })}
-          </p>
-          <KFButton color="primary">Start Workout</KFButton>
+          </div>
+          <KFButton
+            color={"primary"}
+            onClick={isStarted ? undefined : handleStartWorkout}
+            isDisabled={isStarted}
+          >
+            {"Start Workout"}
+          </KFButton>
         </div>
       </div>
     </section>

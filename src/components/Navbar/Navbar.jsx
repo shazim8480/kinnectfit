da56dae2f4fe4eb90c21bedcdac00602abba17a2
shazim8480 <troublemaker8480@gmail.com>
2023-12-28 15:53:46 +0600

    navbar fix

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -9,20 +9,35 @@ import {
   NavbarItem,
   Link,
   Button,
+  DropdownItem,
+  DropdownTrigger,
+  Dropdown,
+  DropdownMenu,
 } from "@nextui-org/react";
+import { useDispatch, useSelector } from "react-redux";
+import { logOutUser } from "@/redux/feature/user/userSlice";
+import { ChevronDown } from "@/assets/icons/ChevronDown";
 
 export default function KFNavbar() {
+  const dispatch = useDispatch();
   const [isMenuOpen, setIsMenuOpen] = React.useState(false);
 
-  const menuItems = [
-    "Features",
-    "Foods",
-    "Plans",
-    "Analytics",
-    "Exercise",
-    "Login",
-    "Sign Up",
-  ];
+  const userProfile = useSelector((state) => state.user);
+  console.log(
+    "🚀 ~ file: Navbar.jsx:19 ~ KFNavbar ~ userProfile:",
+    userProfile
+  );
+
+  let userName = userProfile?.user?.name;
+  // console.log("🚀 ~ file: Navbar.jsx:21 ~ KFNavbar ~ userName:", userName);
+
+  // handle logout
+  const handleLogout = () => {
+    console.log("logged out");
+    dispatch(logOutUser());
+  };
+
+  const menuItems = ["Workout Plans", "Find your Meal"];
 
   return (
     <Navbar isBordered isMenuOpen={isMenuOpen} onMenuOpenChange={setIsMenuOpen}>
@@ -34,48 +49,78 @@ export default function KFNavbar() {
 
       <NavbarContent className="pr-3 sm:hidden" justify="center">
         <NavbarBrand>
-          <Link href="/" className="font-bold text-inherit brand-name">
+          <Link
+            href="/"
+            className="font-bold text-inherit brand-name text-[18px]"
+          >
             KinnectFit
           </Link>
         </NavbarBrand>
       </NavbarContent>
 
-      <NavbarContent className="hidden gap-4 sm:flex" justify="end">
+      <NavbarContent className="hidden sm:flex" justify="center">
         <NavbarBrand>
-          <Link href="/" className={`font-bold text-inherit  text-[18px]`}>
+          <Link href="/" className={`font-bold text-inherit text-[18px]`}>
             KinnectFit
           </Link>
         </NavbarBrand>
         <NavbarItem>
-          <Link href="#">Features</Link>
+          <Link href="#">Workout Plans</Link>
         </NavbarItem>
         <NavbarItem>
-          <Link href="#" aria-current="page">
-            Foods
-          </Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Link href="#">Plans</Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Link href="#">Exercise</Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Link href="#">Analytics</Link>
+          <Link href="#">Find your Meal</Link>
         </NavbarItem>
       </NavbarContent>
 
       <NavbarContent justify="end">
-        <NavbarItem className="hidden lg:flex">
-          <Button as={Link} color="secondary" href="/sign-in" variant="light">
-            Login
-          </Button>
-        </NavbarItem>
-        <NavbarItem>
-          <Button as={Link} color="secondary" href="/sign-up">
-            Sign Up
-          </Button>
-        </NavbarItem>
+        {userProfile?.isAuthenticated === true && (
+          <NavbarContent className="hidden gap-4 sm:flex" justify="center">
+            <Dropdown>
+              <NavbarItem>
+                <DropdownTrigger>
+                  <Button
+                    disableRipple
+                    className="p-0"
+                    endContent={<ChevronDown />}
+                    // radius="sm"
+                    variant="secondary"
+                  >
+                    Hi, Admin
+                  </Button>
+                </DropdownTrigger>
+              </NavbarItem>
+              <DropdownMenu
+                aria-label="Kinnectfit"
+                className="w-[200px]"
+                itemClasses={{
+                  base: "gap-4",
+                }}
+              >
+                <DropdownItem
+                  key="dashboard"
+                  // description="ACME scales apps to meet user demand, automagically, based on load."
+                  // startContent={icons.scale}
+                >
+                  Dashboard
+                </DropdownItem>
+              </DropdownMenu>
+            </Dropdown>
+          </NavbarContent>
+        )}
+
+        {userProfile?.isAuthenticated === true ? (
+          <NavbarItem>
+            <Button onClick={() => handleLogout()} color="secondary">
+              Log out
+            </Button>
+          </NavbarItem>
+        ) : (
+          <NavbarItem>
+            <Button as={Link} color="secondary" href="/sign-in">
+              Join Us
+            </Button>
+          </NavbarItem>
+        )}
       </NavbarContent>
 
       <NavbarMenu>

5d23f07e5342054e28f8117b091a3a5db37bdeb9
Maruf Hossain <dev.maruf112@gmail.com>
2024-01-14 00:23:30 +0600

    redux persist implemented

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,19 +1,32 @@
 "use client";
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import userReducer from "./feature/user/userSlice";
 import { api } from "./api/apiSlice";
 import surveyFormReducer from "./feature/survey/surveySlice";
 import mealPlanReducer from "./feature/meal/mealSlice";
 import workoutReducer from "./feature/workout/workoutSlice";
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
-const store = configureStore({
-  reducer: {
+
+const rootReducer = combineReducers(
+  {
     mealplan: mealPlanReducer,
     workout: workoutReducer,
     user: userReducer,
     surveyForm: surveyFormReducer,
     [api.reducerPath]: api.reducer,
-  },
+  }
+);
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+const store = configureStore({
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(api.middleware),
 });

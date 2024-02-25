7e7358bfa9087eba77c5b372d18115b6af5c2794
whoafridi <afridirk10@gmail.com>
2024-02-26 00:11:58 +0600

    review components updated

--- src/components/ShowReview/ShowReview.jsx
+++ src/components/ShowReview/ShowReview.jsx
@@ -1,32 +1,42 @@
-import Image from 'next/image';
-import ReviewStar from './ReviewStar';
+import Image from "next/image";
+import ReviewStar from "./ReviewStar";
+import HeadingText from "../UI/HeadingText";
+
 const ShowReview = ({ review }) => {
-    return (
-        <div className=" m-10">
-            <div className='grid grid-cols-1 md:grid-cols-2 h-full  min-h-[650px] md:min-h-[350px] items-center shadow-lg '>
-                <div className='p-10'>
-                    <div>
-                        {/* <p>Hello</p> */}
-                        <span className='font-semibold text-lg'>{review?.user?.name}</span>
-                    </div>
-                    <div className='mt-1'>
-                        <ReviewStar rating={review?.rating} />
-                    </div>
-                    <div className='mt-2'>
-                        <span className='font-medium text-base'>{review?.review_item_name}</span>
-                    </div>
-                    <div className='mt-2'>
-                        <span className='italic text-gray-600'>
-                            {review?.review_description}
-                        </span>
-                    </div>
-                </div>
-                <div className='h-full relative '>
-                    <Image src={review?.review_cover[0]} alt='rental' layout='fill' className='absolute' />
-                </div>
-            </div>
-        </div >
-    );
+  return (
+    <>
+      <HeadingText title="User Reviews" className="text-center" />
+      <div className="grid max-w-screen-xl grid-cols-1 grid-rows-1 py-10 mx-auto md:grid-cols-2 shadow-lg mb-10">
+        <div className="p-10">
+          <div>
+            {/* <p>Hello</p> */}
+            <span className="font-semibold text-lg">{review?.user?.name}</span>
+          </div>
+          <div className="mt-1">
+            <ReviewStar rating={review?.rating} />
+          </div>
+          <div className="mt-2">
+            <span className="font-medium text-base">
+              {review?.review_item_name}
+            </span>
+          </div>
+          <div className="mt-2">
+            <span className="italic text-gray-600">
+              {review?.review_description}
+            </span>
+          </div>
+        </div>
+        <div className="h-full relative ">
+          <Image
+            src={review?.review_cover[0]}
+            alt="rental"
+            layout="fill"
+            className="absolute"
+          />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default ShowReview;
